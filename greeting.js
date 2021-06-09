09fd8a9dfaca857043cec7165cfe6491033e3240
hani <akkoahani@gmail.com>
2021-06-09 16:06:59 +0900

    localStorage_name

--- greeting.js
+++ greeting.js
@@ -7,27 +7,48 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
-//실행순서4 -> 최종적으로 웹브라우저에 구현될 기능
+//입력한 값을 저장하는 함수
+function saveName(text){
+  localStorage.setItem(USER_LS,text);
+}
+
+//입력한 값을 처리하는 함수
+function handleSubmit(event){
+  event.preventDefault();//default기능 없애기
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+
+//이름 입력하는 함수
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+
+// 최종적으로 웹브라우저에 구현될 기능
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerHTML = `Hello ${text}`;
 }
 
-//실행순서 3 -> currentUser의 value가 있으면 paintGreeting 호출
+//currentUser의 value가 있으면 paintGreeting 호출
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
 }
 
-//실행순서 2 -> loadName()메소드 호출
+//loadName()메소드 호출
 function init() {
   loadName();
 }
 
-//실행 순서 1
+//최종 실행
 init();
