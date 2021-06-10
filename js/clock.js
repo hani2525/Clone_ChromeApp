ede650d0d02c5ab276efc407c7b90dd0182d7fa9
hani <akkoahani@gmail.com>
2021-06-10 21:46:54 +0900

    Making clock using padStart

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,15 @@
 const clock = document.querySelector("#clock");
 const clockTitle = document.querySelector("h1");
 
-function something(){
+function getClock(){
+    const date = new Date();
+  const hour = String(date.getHours()).padStart(2,0);
+  const minute = String(date.getMinutes()).padStart(2,0);
+  const second = String(date.getSeconds()).padStart(2,0);
 
+  
+  clock.innerText = `${hour}:${minute}:${second}`;
 }
 
-setInterval(something, 1000);
+getClock();
+setInterval(getClock, 1000);
