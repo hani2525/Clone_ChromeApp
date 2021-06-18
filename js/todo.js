88baacd0942e2b4bbd9015851a8324fb60b13c69
hani <akkoahani@gmail.com>
2021-06-18 21:15:50 +0900

    Saving Todos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDOList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -22,7 +27,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
